e107dd93b84cf40c0aebf33b793f50883b9f0020
itulupov1 <itulupov1@yandex.ru>
2020-08-23 20:17:47 +0700

    leeson12

---
+++ js/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+const arr = JSON.parse(localStorage.getItem('arr'));
+const todoData = arr;
+
+const render = function() {
+	todoList.textContent = '';
+	todoCompleted.textContent = '';
+
+	todoData.forEach(function(item){
+		const li = document.createElement('li');
+		li.classList.add('todo-item');
+
+		li.innerHTML = '<span class="text-todo">' + item.value +'</span>' + 
+			'<div class="todo-buttons">' +
+				'<button class="todo-remove"></button>' +
+				'<button class="todo-complete"></button>' +
+			'</div>';
+		if (item.completed) {
+			todoCompleted.append(li);
+		} else {
+			todoList.append(li);
+		}
+
+		const btnTodoComplete = li.querySelector('.todo-complete');
+
+		btnTodoComplete.addEventListener('click', function(){
+			item.completed = !item.completed;
+			render();
+		});
+		
+		const btnTodoRemove = li.querySelector('.todo-remove');
+
+		btnTodoRemove.addEventListener('click', function (){
+
+			for (let i = 0; i < todoData.length; i++){
+				if (todoData[i].value === item.value) {
+					todoData.splice(i, 1);
+					localStorage.clear();
+					localStorage.setItem('arr', JSON.stringify(todoData));
+				}
+			}
+			render();
+		});
+		// localStorage.clear();
+		// localStorage.setItem('kek', JSON.stringify(todoData));
+	});
+};
+
+todoControl.addEventListener('submit', function(event){
+	event.preventDefault();
+
+	if (headerInput.value !== ''){
+		const newTodo = {
+			value: headerInput.value,
+			completed: false
+		};
+		todoData.push(newTodo);
+		headerInput.value = '';
+	} 
+	render();
+	localStorage.clear();
+	localStorage.setItem('arr', JSON.stringify(todoData));
+});
+
+render();
+
+// localStorage.setItem(todoData, JSON.stringify(todoData));
+// todoData = JSON.parse(localStorage.getItem("todoData"));
